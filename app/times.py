cfcd09074fb2393f938f24b86fe9295b809f2b8c
Roy Rosemarin <roy@amropartners.com>
2022-11-15 14:16:51 +0000

    merge

--- app/times.py
+++ app/times.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 def utc_now():
-    return datetime(2022, 11, 15, 0, 0)  ################  TODO: change back to datetime.utcnow()
+    return datetime.utcnow()
 
 
 @st.experimental_singleton
