2deb071ac0a4e661b2469145910e32886978c62e
roy-rosemarin <110601979+roy-rosemarin@users.noreply.github.com>
2022-12-13 17:26:27 +0000

    Update firebase.py

--- app/firebase.py
+++ app/firebase.py
@@ -9,10 +9,10 @@ from google.oauth2 import service_account
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
-def get_db_from_firebase_key():
+def get_db_from_firebase_key(storage_bucket):
     key_dict = json.loads(st.secrets["firebase_key"])
     creds = service_account.Credentials.from_service_account_info(key_dict)
-    firebase_admin.initialize_app(creds, {'storageBucket': cnf.storage_bucket})
+    firebase_admin.initialize_app(creds, {'storageBucket': storage_bucket})
     return firestore.Client(credentials=creds, project="amro-partners")
 
 
